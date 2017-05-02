64392198b96954b8bd5a7ec8e694332969d71af7
mahadyC <mahady.chowdhury@metropolia.fi>
2017-05-02 14:13:54 +0300

    Introduce currentCat property to hold selected cat object. As the property would hold data thus it is intoduced inside model which holds all the  data of any application.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $('document').ready(function() {
     var model = {
+        currentCat: null,
         cats: [
             {
                 clickCount: 0,
@@ -55,6 +56,7 @@ $('document').ready(function() {
         // image: '',
         // clickCounter: ''
     };
+
 
 
 
