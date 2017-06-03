5248b3d535039f5a47b7098169680d8f10597998
mahadyC <mahady.chowdhury@metropolia.fi>
2017-06-03 14:23:02 +0300

    Clean up.

--- main.js
+++ main.js
@@ -29,9 +29,7 @@ $('document').ready(function() {
                 image: 'image/cat4.jpg'
             }
         ]
-        // names: ['mini', 'bini', 'sini', 'gini', 'yini'],
-        // images: ['image/cat.jpg', 'image/cat1.jpg', 'image/cat2.jpg', 'image/cat3.jpg', 'image/cat4.jpg'],
-        // counter: [0, 0, 0, 0, 0]
+        
     };
 
     var catListView = {
@@ -54,16 +52,13 @@ $('document').ready(function() {
             
         }
     }
-        // item: $('li'),
-        // heading: '',
-        // image: '',
-        // clickCounter: ''
+        
     };
 
     var catView = {
        
         render: function(){
-              // Incrementing function
+              
               var currentCat = octopus.getCurrentCat();
               
             // render function for the selected/clicked image
@@ -85,6 +80,7 @@ $('document').ready(function() {
     };
 
     var adminView = {
+        
         init: function(){
             $('#admin').click(function(){
                 octopus.showAdminPanel();
@@ -103,6 +99,7 @@ $('document').ready(function() {
 
 
     var octopus = {
+        
         init: function(){
             catListView.render();
             adminView.init();
@@ -143,70 +140,5 @@ $('document').ready(function() {
 
     };
     octopus.init();
-
-
-    // var items = [$('#mini'), $('#bini'), $('#sini'), $('#gini'), $('#yini')];
-    // console.log(items[0]);
-    // var counter1 = 0;
-    // var counter2 = 0;
-    // var counter3 = 0;
-    // var counter4 = 0;
-    // var counter5 = 0;
-
-
-    // items[0].click(function(){
-    //     counter1 = counter1 + 1;
-    //     $('#headingId').remove();
-    //     $('#catName').remove();
-    //     $('#image1').remove();
-    //     $('div').append('<h2 id="catName">Mini</h2>');
-    //     $('div').append('<h2 id="headingId">Number of clicks:' + counter1 + '</h2>');
-    //     $('div').append('<img id="image1" src="https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426">');
-    // });
-
-    // items[1].click(function(){
-    //     counter2 = counter2 + 1;
-    //     $('#headingId').remove();
-    //     $('#catName').remove();
-    //     $('#image1').remove();
-    //     $('div').append('<h2 id="catName">Bini</h2>');
-    //     $('div').append('<h2 id="headingId">Number of clicks:' + counter2 + '</h2>');
-    //     $('div').append('<img id="image1" src="https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426">');
-
-    // });
-
-    // items[2].click(function(){
-    //     counter3 = counter3 + 1;
-    //     $('#headingId').remove();
-    //     $('#catName').remove();
-    //     $('#image1').remove();
-    //     $('div').append('<h2 id="catName">Sini</h2>');
-    //     $('div').append('<h2 id="headingId">Number of clicks:' + counter3 + '</h2>');
-    //     $('div').append('<img id="image1" src="https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426">');
-
-    // });
-
-    // items[3].click(function(){
-    //     counter4 = counter4 + 1;
-    //     $('#headingId').remove();
-    //     $('#catName').remove();
-    //     $('#image1').remove();
-    //     $('div').append('<h2 id="catName">Gini</h2>');
-    //     $('div').append('<h2 id="headingId">Number of clicks:' + counter4 + '</h2>');
-    //     $('div').append('<img id="image1" src="https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426">');
-
-    // });
-
-    // items[4].click(function(){
-    //     counter5 = counter5 + 1;
-    //     $('#headingId').remove();
-    //     $('#catName').remove();
-    //     $('#image1').remove();
-    //     $('div').append('<h2 id="catName">Yini</h2>');
-    //     $('div').append('<h2 id="headingId">Number of clicks:' + counter5 + '</h2>');
-    //     $('div').append('<img id="image1" src="https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426">');
-
-    // });
-
 
 });
